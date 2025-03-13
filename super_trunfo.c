2723566d1f12b6a19abf78566bacd79ef2e61082
Guilherme Krainski de Oliveira <202501114962@alunos.estacio.br>
2025-03-13 10:18:14 -0300

    Atualizar o super_trunfo.c
    Substituidos operadores de comentários de /**/ para // e pequenos ajustes de quebra de linha e tabs

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,69 +6,69 @@ int main(){
     int populacao1, pontos_turisticos1, populacao2, pontos_turisticos2;
     float pib1, area1, pib2, area2;
 
-    
-    /*Mensagem inicial para o usuário do programa*/
-    printf("============Sistema de Cadastro de Cartas Super-Trunfo============\n\nNotas do desenvolvedor:\n");
-    printf("-Utilize apenas caracteres simples!\n-Se o nome for composto use underline(_) no lugar de espaço!\n\n");
 
-    /*================ Bloco de Cadastro de itens da carta 1 ================*/
-    printf("Carta 1: \n");
-    printf("Insira o Estado: "); 
+    //Mensagem inicial para o usuário do programa
+    printf("============Sistema de Cadastro de Cartas Super-Trunfo============\n\nNotas do desenvolvedor:\n\n");
+    printf("    -Utilize apenas caracteres simples!\n    -Se o nome for composto use underline(_) no lugar de espaço!\n\n");
+
+    //================ Bloco de Cadastro de itens da carta 1 ================
+    printf("Carta 1: \n\n");
+    printf("Insira o Estado: \n"); 
     scanf(" %s", &estado1); 
 
-    printf("Insira o código da carta com até 4 dígitos: "); 
+    printf("Insira o código da carta com até 4 dígitos: \n"); 
     scanf(" %s", &codigo1); 
-    
-    printf("Insira o nome da cidade: "); 
+
+    printf("Insira o nome da cidade: \n"); 
     scanf(" %s", &cidade1); 
 
-    printf("Insira o numero de habitantes da cidade: ");
+    printf("Insira o numero de habitantes da cidade: \n");
     scanf(" %d", &populacao1);
 
-    printf("Insira o numero de pontos turisticos nesse local: ");
+    printf("Insira o numero de pontos turisticos nesse local: \n");
     scanf(" %d", &pontos_turisticos1);
 
-    printf("Agora o PIB deste local: ");
+    printf("Agora o PIB deste local: \n");
     scanf(" %f", &pib1);
 
-    printf("Por ultimo insira a área deste local em km²: ");
+    printf("Por ultimo insira a área deste local em km²: \n");
     scanf(" %f", &area1);
     printf("\n");
-    /*================ Bloco de Cadastro de itens da carta 1 ================*/
+    //================ Bloco de Cadastro de itens da carta 1 ================
 
 
 
-    /*Informativo ao usuário que o segundo bloco está sendo iniciado*/
+    //Informativo ao usuário que o segundo bloco está sendo iniciado
     printf("============ Cadastro da Segunda Carta ============\n\n");
 
 
-    /*================ Bloco de Cadastro de itens da carta 2 ================*/
+    //================ Bloco de Cadastro de itens da carta 2 ================
     printf("Carta 2: \n");
-    printf("Insira o Estado: "); 
+    printf("Insira o Estado: \n"); 
     scanf(" %s", &estado2); 
 
-    printf("Insira o código da carta com até 4 dígitos: "); 
+    printf("Insira o código da carta com até 4 dígitos: \n"); 
     scanf(" %s", &codigo2); 
 
-    printf("Insira o nome da cidade: "); 
+    printf("Insira o nome da cidade: \n"); 
     scanf(" %s", &cidade2); 
 
-    printf("Insira o numero de habitantes da cidade: ");
+    printf("Insira o numero de habitantes da cidade: \n");
     scanf(" %d", &populacao2);
 
-    printf("Insira o numero de pontos turisticos nesse local: ");
+    printf("Insira o numero de pontos turisticos nesse local: \n");
     scanf(" %d", &pontos_turisticos2);
 
-    printf("Agora o PIB deste local: ");
+    printf("Agora o PIB deste local: \n");
     scanf(" %f", &pib2);
 
 
-    printf("Por ultimo insira a área deste local: ");
+    printf("Por ultimo insira a área deste local: \n");
     scanf(" %f", &area2);
 
     printf("\n---====FIM DO CADASTRO===---");
-    /*================ Bloco de Cadastro de itens da carta 2 ================*/
-    
+    //================ Bloco de Cadastro de itens da carta 2 ================
+
     printf("\n============== CARTA 1 ==============\n");
     printf("Estado: %s\n", estado1);
     printf("Código: %s\n", codigo1);
@@ -77,7 +77,7 @@ int main(){
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Pontos turísticos: %d\n", pontos_turisticos1);
-    
+
 
     printf("\n============== CARTA 2 ==============\n");
     printf("Estado: %s\n", estado2);
@@ -89,6 +89,6 @@ int main(){
     printf("Pontos turísticos: %d\n", pontos_turisticos2);
     printf("\n=====================================\n");
 
-    
+
 return 0;
 }
